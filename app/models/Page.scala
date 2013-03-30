8fe6d8d9a655de9f1d9ee665f9087f2bba8fa99a
Jason Buffin <jason@loverofmysoul.com>
2013-03-30 10:54:25 -0400

    modularized the template landing page

---
+++ app/models/Page.scala
@@ -0,0 +1,8 @@
+package models
+
+case class Page(
+		)
+
+object Page {
+
+}
---
+++ app/models/widgets/Text.scala
@@ -0,0 +1,12 @@
+package models.widgets
+
+case class Text(
+		title: String,
+		text: String,
+		id: Long = -1)
+
+object Text {
+	
+	
+	
+}
