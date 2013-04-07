5021b03fe3d1d117a0c2b430884a299b2f3d7f4e
Jason Buffin <jason@loverofmysoul.com>
2013-04-06 22:59:01 -0400

    this commit changed a lot of structure and Sites controller is currently broken as well as the landing page

---
+++ app/models/widgets/WidgetType.scala
@@ -0,0 +1,56 @@
+package models.widgets
+
+import play.api.db._
+import play.api.Play.current
+import anorm._
+import anorm.SqlParser._
+
+case class WidgetType(widgetType: String,
+	typeId: Long)
+
+object WidgetType {
+
+	val simple = {
+		get[String]("widget_types.type_name") ~
+		get[Long]("widget_types.type_id") map {
+			case type_name ~ type_id => WidgetType(type_name, type_id)
+		}
+	}
+
+	def getAll: List[WidgetType] = {
+		DB.withConnection { implicit connection =>
+			SQL(
+				"""
+					select * from widget_types
+				"""
+			).as(WidgetType.simple *)
+		}
+	}
+	
+	def getByTypeName(typeName: String): Option[WidgetType] = {
+		DB.withConnection { implicit connection =>
+			SQL(
+				"""
+					select * from widget_types
+						where type_name = {type_name}
+				"""
+			).on(
+				'type_name -> typeName
+			).as(WidgetType.simple.singleOpt)
+		}
+	}
+	
+	def getById(typeId: Long): Option[WidgetType] = {
+		DB.withConnection { implicit connection =>
+			SQL(
+				"""
+					select * from widget_types
+						where type_id = {type_id}
+				"""
+			).on(
+				'type_id -> typeId
+			).as(WidgetType.simple.singleOpt)
+		}
+	}
+
+}
---
+++ conf/evolutions/default/2.sql
@@ -0,0 +1,25 @@
+# public schema
+ 
+# --- !Ups
+  
+CREATE TABLE widget_types
+(
+   type_id bigserial NOT NULL,
+   type_name varchar(255) NOT NULL,
+   CONSTRAINT type_id PRIMARY KEY (type_id)
+);
+
+INSERT INTO widget_types (type_name) VALUES ('TEXT');
+INSERT INTO widget_types (type_name) VALUES ('CAROUSEL');
+
+CREATE TABLE page_widgets
+(
+	page_widget_id bigserial NOT NULL,
+	page_id bigint NOT NULL,
+	widget_id bigint NOT NULL
+	CONSTRAINT page_widget_id PRIMARY KEY (page_widget_id)
+);
+
+# --- !Downs
+DROP TABLE IF EXISTS widget_types;
+DROP TABLE IF EXISTS page_widgets;
