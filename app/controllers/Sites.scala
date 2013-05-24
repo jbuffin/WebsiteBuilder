90a5695e48b6d210b2a6e0d427fe5d67af5d3c55
Jason Buffin <jason@loverofmysoul.com>
2013-05-24 18:57:19 -0400

    removed the Sites.index method. the controller just calls the getPageFromUri method with the uri supplied as an empty string. added a getChildPage method to prepare for displaying child pages. it parses the uri into a list of all the page names

--- app/controllers/Sites.scala
+++ app/controllers/Sites.scala
@@ -8,14 +8,11 @@ import play.api.templates.Html
 
 object Sites extends Controller {
 
-	def index = {
-		getPageFromUri("")
-	}
-	/*Action { implicit request =>
-		Logger.debug("[Sites.index]: request.domain: '"+request.domain+"'")
+	def getPageFromUri(uri: String = "") = Action { implicit request =>
+		Logger.debug("[Sites.getPageFromUri]: request.domain: '"+request.domain+"', uri: '"+uri+"'")
 		try {
 			val site = Site.getSiteByHostName(request.domain).get
-			val page = Page.getPageByUri(site.siteId).get
+			val page = Page.getPageByUri(site.siteId, uri).get
 			val listOfWidgets: List[Long] = Widgets.getWidgetList(page.pageId)
 			if (listOfWidgets.length != 3) {
 				throw new NoSuchElementException
@@ -29,18 +26,14 @@ object Sites extends Controller {
 				Redirect(routes.Application.indexWithNoSiteFound)
 			}
 		}
-	}*/
+	}
 
-	def getPageFromUri(uri: String) = Action { implicit request =>
-		Logger.debug("[Sites.getPageFromUri]: request.domain: '"+request.domain+"', uri: '"+uri+"'")
+	def getChildPage(page: String) = Action { implicit request =>
+		val parsedPages = page.split("/").toList
 		try {
 			val site = Site.getSiteByHostName(request.domain).get
-			val page = Page.getPageByUri(site.siteId, uri).get
-			val listOfWidgets: List[Long] = Widgets.getWidgetList(page.pageId)
-			if (listOfWidgets.length != 3) {
-				throw new NoSuchElementException
-			}
-			Ok(views.html.sites.templates.gracechurch.index(page.title, listOfWidgets))
+
+			Ok(parsedPages.last)
 		}
 		catch {
 			case nse: NoSuchElementException => {
