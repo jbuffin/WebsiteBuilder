d4f660d0a8d13c61c6e0e892035cfc9643c120b8
Jason Buffin <jason@loverofmysoul.com>
2013-06-03 07:14:12 -0400

    drop downs for sites, pages, widgets, wired up with knockout

--- public/javascripts/admin/admin.js
+++ public/javascripts/admin/admin.js
@@ -9,20 +9,45 @@ function AdminViewModel() {
 	self.currentSite = ko.observable();
 	self.currentPage = ko.observable();
 	self.currentWidget = ko.observable();
+	self.siteList = ko.observableArray([]);
+	self.pageList = ko.observableArray([]);
+	self.widgetList = ko.observableArray([]);
+
+	self.currentSite.subscribe(function(newValue) {
+		if(newValue) {
+			pageServer.getAllBySite(newValue.id, function(data) {
+				self.pageList(data);
+			});
+		} else {
+			self.pageList([]);
+		}
+	});
+
+	self.currentPage.subscribe(function(newValue) {
+		if(newValue) {
+			widgetServer.getAllByPage(newValue.id, function(data) {
+				self.widgetList(data);
+			});
+		} else {
+			self.widgetList([]);
+		}
+	});
 
 	self.init = function() {
 		server = new FakeServerAccessor("");
 		siteServer = new SiteAccessor(server);
 		pageServer = new PageAccessor(server);
 		widgetServer = new WidgetAccessor(server);
+		siteServer.getAll(function(data) {
+			self.siteList(data);
+		});
 	}
 }
 
-var avm;
-avm = new AdminViewModel();
+var avm = new AdminViewModel();
 avm.init();
 $(function() {
-	if (typeof ko !== "undefined") {
+	if (ko) {
 		ko.applyBindings(avm);
 	}
 });
@@ -32,7 +57,15 @@ function SiteAccessor(server) {
 	self.server = server;
 
 	self.getAll = function(callback) {
-
+		callback([ {
+			id : 1,
+			siteName : 'grace church',
+			hostName : 'gracechurch.net'
+		}, {
+			id : 2,
+			siteName : 'my church site',
+			hostName : 'mychurchsite.com'
+		} ]);
 	};
 	self.newSite = function(site, callback) {
 
@@ -42,8 +75,17 @@ function PageAccessor(server) {
 	var self = this;
 	self.server = server;
 
-	self.getAllBySite = function(callback) {
-
+	self.getAllBySite = function(siteId, callback) {
+		callback([ {
+			id : 1,
+			title : 'Default Page'
+		}, {
+			id : 2,
+			title : 'Our Church'
+		}, {
+			id : 3,
+			title : 'Directions'
+		} ]);
 	};
 	self.newPage = function(page, callback) {
 
@@ -53,8 +95,16 @@ function WidgetAccessor(server) {
 	var self = this;
 	self.server = server;
 
-	self.getAllByPage = function(callback) {
-
+	self.getAllByPage = function(pageId, callback) {
+		callback([ {
+			id : 1
+		}, {
+			id : 2
+		}, {
+			id : 3
+		}, {
+			id : 4
+		} ]);
 	};
 	self.newWidget = function(page, callback) {
 
